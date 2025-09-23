54bdadf6459a769ac4b81b2e05a561cd707515b9
ayaSaleh717 <105351734+ayaSaleh717@users.noreply.github.com>
2025-09-23 18:22:49 +0300

    first commit

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -1,84 +1,94 @@
 "use client";
-import React, { useTransition, useState } from "react";
+import React, {useTransition, useState} from "react";
 import Image from "next/image";
 import TabButton from "./TabButton";
 
 const TAB_DATA = [
-  {
-    title: "Skills",
-    id: "skills",
-    content: (
-      <ul className="list-disc pl-2">
-        <li> Html | css & Bootstrap Tailwind css .</li>
-         <li> Javascript & Typescript</li>
-        <li>React.js & Next.js</li>
-        <li>Node.js & Express</li>
-        <li>React Hooks, React
-        Router , Redux, Redux Toolkit</li>
-        <li> Git & Github</li>
-        <li>SQL and MongoDb </li>       
-        <li>Testing/Debugging Tools</li>
-        <li> Package Managers</li>
-      </ul>
-    ),
-  },
-  {
-    title: "Education",
-    id: "education",
-    content: (
-      <ul className="list-disc pl-2">
-        <li>Bachelor of Software Enginering at  Tishreen University</li>
-      </ul>
-    ),
-  },
- 
+    {
+        title: "Skills",
+        id: "skills",
+        content: (
+            <ul className="list-disc pl-2">
+                <li>
+                    Html | css & Bootstrap Tailwind css .</li>
+                <li>
+                    Javascript & Typescript</li>
+                <li>React.js & Next.js</li>
+                <li>Node.js & Express</li>
+                <li>React Hooks, React
+                            Router , Redux, Redux Toolkit</li>
+                <li>
+                    Git & Github</li>
+                <li>SQL and MongoDb
+                </li>
+                <li>Testing/Debugging Tools</li>
+                <li>
+                    Package Managers</li>
+            </ul>
+        )
+    }, {
+        title: "Education",
+        id: "education",
+        content: (
+            <ul className="list-disc pl-2">
+                <li>Bachelor of Software Enginering at  Tishreen University</li>
+            </ul>
+        )
+    },
+
 ];
 
 const AboutSection = () => {
-  const [tab, setTab] = useState("skills");
-  const [isPending, startTransition] = useTransition();
+    const [tab, setTab] = useState("skills");
+    const [isPending, startTransition] = useTransition();
 
-  const handleTabChange = (id) => {
-    startTransition(() => {
-      setTab(id);
-    });
-  };
+    const handleTabChange = (id) => {
+        startTransition(() => {
+            setTab(id);
+        });
+    };
 
-  return (
-    <section className="text-white" id="about">
-      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image src="/images/about-image.png" width={500} height={500} alt="About illustration" />
-        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
-          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
-          <p className="text-base lg:text-lg">
-          Hi, I&apos;m Aya
+    return (
+        <section className="text-white" id="about">
+            <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+                <Image className="hidden md:block" src="/images/about-image.png"
+                    width={500}
+                    height={500}
+                    alt="About illustration"/>
+                <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+                    <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+                    <p className="text-base lg:text-lg">
+                        Hi, I&apos;m Aya
+                        
+                        A Front-End Developer who builds modern, responsive web applications with HTML, CSS, JavaScript, and React. I love turning complex problems into simple, elegant solutions through clean code and creative design. Let&apos;s create something amazing together.
+                    </p>
+                    <div className="flex flex-row justify-start mt-8">
+                        <TabButton selectTab={
+                                () => handleTabChange("skills")
+                            }
+                            active={
+                                tab === "skills"
+                        }>
+                            {" "}
+                            Skills{" "} </TabButton>
+                        <TabButton selectTab={
+                                () => handleTabChange("education")
+                            }
+                            active={
+                                tab === "education"
+                        }>
+                            {" "}
+                            Education{" "} </TabButton>
 
-A Front-End Developer who builds modern, responsive web applications with HTML, CSS, JavaScript, and React. I love turning complex problems into simple, elegant solutions through clean code and creative design. Let&apos;s create something amazing together.
-          </p>
-          <div className="flex flex-row justify-start mt-8">
-            <TabButton
-              selectTab={() => handleTabChange("skills")}
-              active={tab === "skills"}
-            >
-              {" "}
-              Skills{" "}
-            </TabButton>
-            <TabButton
-              selectTab={() => handleTabChange("education")}
-              active={tab === "education"}
-            >
-              {" "}
-              Education{" "}
-            </TabButton>
-           
-          </div>
-          <div className="mt-8">
-            {TAB_DATA.find((t) => t.id === tab).content}
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+                    </div>
+                    <div className="mt-8">
+                        {
+                        TAB_DATA.find((t) => t.id === tab).content
+                    } </div>
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default AboutSection;
