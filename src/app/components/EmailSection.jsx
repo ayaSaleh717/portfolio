9f9d8ac5c085674abb01825347d2d6eaa04e0b0b
ayaSaleh717 <105351734+ayaSaleh717@users.noreply.github.com>
2025-09-23 19:55:42 +0300

    first commit

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -86,9 +86,7 @@ const EmailSection = () => {
         <Image src="/images/connect.png" alt="hero image" className=""
                     width={300}
                     height={300}/>
-                      <Image src="/images/p8.PNG" alt="hero image" className=""
-                    width={300}
-                    height={300}/>
+                     
         
       </div>
       
