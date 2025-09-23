9aed956178d47449789d0a08183ebf05cf9d3191
ayaSaleh717 <105351734+ayaSaleh717@users.noreply.github.com>
2025-09-23 20:17:34 +0300

    first commit

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -44,11 +44,11 @@ const HeroSection = () => {
               Hire Me
             </Link>
             <Link
-              href="/"
+              href="https://drive.google.com/file/d/123k4pXMYwO43h57s2o-mTIuQBfvVQuzK/view?usp=sharing"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-cyan-500 to-cyan-600 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
-               <Link href="https://drive.google.com/file/d/123k4pXMYwO43h57s2o-mTIuQBfvVQuzK/view?usp=sharing">Download CV</Link>
+                Download CV
               </span>
             </Link>
           </div>
